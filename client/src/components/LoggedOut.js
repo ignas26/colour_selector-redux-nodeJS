5ceb35ce4dba6ca9d830cb706c9d78de31c80b9e
ignas26 <ignas.zlatkus@gmail.com>
2018-08-04 01:57:02 +0300

    fixed loggedOut component to properly display whether the user is logged in or not

--- client/src/components/LoggedOut.js
+++ client/src/components/LoggedOut.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import {connect} from "react-redux";
 import * as actions from "../actions/user";
+import {Link} from 'react-router-dom';
 
 
 class LoggedOut extends React.Component {
+state={
+  temp:"Vartotojas admin yra neprisijungęs"
+};
 
   render() {
 
-    return ( <loggedOut>
-          <div className="logout">
+    return ( <div>
+          <div className="loggout">
             {this.props.user.name &&
-            <button className="logout-btn"
+            <button className="loggout-btn"
                     onClick={() => {
                       this.props.logout();
                     }}>Logout
             </button>
             }
           </div>
-          <div className="logged-in">{this.props.user.name ?
-              <h1>Jūs esate prisijungę kaip <span> {this.props.user.name} </span></h1> : null} </div>
-      <h2 className="message">{this.props.user.message}</h2>
+          <div className="loggedIn"> {this.props.user.name ?
+              <h1>Jūs esate prisijungę kaip <span> {this.props.user.name} </span></h1> : <Link to={'/login'} className="notLogged-button"><button> Prisijungti</button> </Link>} </div>
           <div className="clearfix"></div>
-        </loggedOut>
+        </div>
     )
   };
 }
 
-
-
 const mapStateToProps=(state)=>{
   return{user:state.user}
 };
-
 
 export default connect(mapStateToProps, actions)(LoggedOut);
